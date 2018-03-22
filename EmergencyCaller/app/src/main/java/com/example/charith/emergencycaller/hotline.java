4266690ac824b8be45babe4a8a2f2f865990a892
Charith071 <33716146+Charith071@users.noreply.github.com>
2018-03-22 19:58:04 +0530

    sads

--- EmergencyCaller/app/src/main/java/com/example/charith/emergencycaller/hotline.java
+++ EmergencyCaller/app/src/main/java/com/example/charith/emergencycaller/hotline.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 public class hotline extends AppCompatActivity {
     ListView listView;
     EditText serch_txt;
+
     String[] name={"sadsda","dsad","sdadfdf","trst","sfadfsdf","sdadfsdfsdfsdf","sdfadfsdfsdfsd","sfsdfsdfsdf",
     "text1","test2","test3","test4","test5","test6"};
     @Override
@@ -20,7 +21,7 @@ public class hotline extends AppCompatActivity {
         setContentView(R.layout.activity_hotline);
       //  serch_txt=findViewById(R.id.hotlineserchtxt_id);
         listView=findViewById(R.id.hot_listview_id);
-
+        
         set_list_item();
     }
 
@@ -30,7 +31,7 @@ public class hotline extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String value= String.valueOf(listView.getItemIdAtPosition(position));
+                String value= String.valueOf(listView.getItemAtPosition(position));
                 Toast.makeText(getApplicationContext(),value,Toast.LENGTH_LONG).show();
             }
         });
